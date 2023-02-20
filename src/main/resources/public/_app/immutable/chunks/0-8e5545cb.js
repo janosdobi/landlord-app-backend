0ddfd43761365e8f82c2629bed420fefa3a3fae2
Janos Dobi <j.dobi88@gmail.com>
2023-02-20 23:49:11 +0100

    fix url

--- src/main/resources/public/_app/immutable/chunks/0-8e5545cb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/layout.svelte-21e52b6a.js";export{t as component};
--- src/main/resources/public/_app/immutable/chunks/1-6a6ef168.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-b85b7d6a.js";export{t as component};
